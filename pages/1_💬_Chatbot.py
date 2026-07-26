d44d20171bf4212d89bb526e4aeaa78cbfab7508
Jenocus <jenoxuan@gmail.com>
2026-07-26 18:11:23 +0800

    resolving password

--- pages/1_💬_Chatbot.py
+++ pages/1_💬_Chatbot.py
@@ -1,4 +1,11 @@
+import os
+import sys
 import streamlit as st
+
+ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
+if ROOT_DIR not in sys.path:
+    sys.path.insert(0, ROOT_DIR)
+
 from helper_functions.llm import get_completion_stream, count_tokens
 from helper_functions.utility import check_password
 
